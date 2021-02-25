8fba9e0458166c7504b2424dc60c1e543ac13bec
github username <github email address>
2021-02-24 23:16:31 -0700

    added url redirect on socials

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -1,9 +1,10 @@
 <template>
   <section class="links">
-      <img class="img-link" src="../assets/ig-logo-2.jpg">
-      <img class="img-link" src="../assets/github-logo.png">
-      <img class="img-link" src="../assets/linkedin-logo.png">
-      <img class="img-link" src="../assets/youtube-logo3.png">
+      <!-- <a href="https://www.instagram.com/davidattenborough/"><img class="img-link" src="../assets/ig-logo-2.jpg"></a> -->
+      <a href="https://www.github.com/nickdongoske"><img class="img-link" src="../assets/github-logo.png"></a>
+      <a href="https://www.linkedin.com/in/nicholas-dongoske-7b03a7157/"><img class="img-link" src="../assets/linkedin-logo.png"></a>
+      <!-- <img class="img-link" src="../assets/youtube-logo3.png"> -->
+     <a href="https://open.spotify.com/playlist/37i9dQZF1Epm7B2mUsVALv?si=4Peei9aIQpilJ94d9O2dMQ"><img class="img-link" src="../assets/spotify-logo.svg" v=""></a>
       
       
 
