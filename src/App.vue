3cd01ff9fb8900db9b0008f53c3bdf7ceb9040a5
github username <github email address>
2021-03-11 13:40:27 -0700

    did some shit

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,5 @@
 <template>
-  <Home msg="Nick Dongoske"/>
-  <Image/>
-  <Links/>
-  <Navigation/>
+  <div id="app">
+
+  </div>
 </template>
-
-<script>
-import Home from './components/Home.vue'
-// import About from './components/About.vue'
-import Image from './components/Image.vue'
-import Links from './components/Links.vue'
-import Navigation from './components/Navigation.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Home,
-    // About,
-    Image,
-    Links,
-    Navigation,
-  }
-}
-</script>
-
-<style>
- #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px; 
-
- } 
-</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+
+
+<template>
+  
+</template>
+
+
+<script>
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+</script>
